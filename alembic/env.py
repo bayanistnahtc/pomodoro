329a5b958413b529430a51b042f54821650cf5a2
bayanistnahtc <bayanistnahtc@gmail.com>
2025-03-07 13:41:57 +0300

    added db and cache values to settings; fixed database, alembic, cache accessors

--- alembic/env.py
+++ alembic/env.py
@@ -1,5 +1,4 @@
 from logging.config import fileConfig
-from os import environ
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
@@ -7,12 +6,15 @@ from sqlalchemy import pool
 from alembic import context
 
 from database.models import Base
+from settings import Settings
+
+settings = Settings()
 
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option("DATABASE_URL", environ.get("DATABASE_URL"))
+config.set_main_option("DATABASE_URL", settings.db_url)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
--- database/database.py
+++ database/database.py
@@ -4,8 +4,7 @@ from sqlalchemy.orm import sessionmaker, Session
 from settings import Settings
 
 setttings = Settings()
-DB_URL = 'postgresql+psycopg2://postgres:password@localhost:5432/pomodoro'
-engine = create_engine(DB_URL, echo=True)
+engine = create_engine(setttings.db_url, echo=True)
 
 
 def get_db_session() -> Session:
